4e11fe610220a6145a1215828bb56214e47a02fa
I <jfl@Jean-Francoiss-MacBook-Pro.local>
2018-01-08 09:18:13 -0500

    improve error handling

--- cli.py
+++ cli.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding: utf8
-
+from __future__ import absolute_import
 import click
+
 from providers import Providers
+
 
 providers = Providers()
 
@@ -11,18 +13,25 @@ providers = Providers()
 @click.option('--service', '-s', type=click.Choice(providers.SERVICES))
 def cli(address, service=None):
     if service:
-        location_response = providers.get(service, address)
+        response = providers.get(service, address)
     else:
-        location_response = providers.get_with_fallback(address)
+        response = providers.get_with_fallback(address)
 
-    print(get_output(location_response))
+    print(gen_output(response))
+
+def gen_output(response):
+    if response.is_success:
+        return response.data
     
-
-def get_output(location_response):
-    if location_response.is_success:
-        return location_response.data
+    if response.status == 'Bad Request':
+        return 'Error: Invalid address.'
+    elif response.status == 'No Results':
+        return 'Error: No results found.'
+    elif response.status == 'Not Found':
+        return 'Error: None of the service providers could be reached.'
     else:
-        return location_response.status
+        return 'Error: ' + response.status
+
 
 if __name__ == "__main__":
     cli()
