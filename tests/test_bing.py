45f07f95f47a27bd2a8f1fbf05519c09b69f37fd
I <jfl@Jean-Francoiss-MacBook-Pro.local>
2018-01-08 00:04:50 -0500

    add unit tests for each service provider

---
+++ tests/test_bing.py
@@ -0,0 +1,19 @@
+import unittest
+from providers import Providers
+
+class TestBing(unittest.TestCase):
+
+    def test_bing_get_not_found(self):
+        p = Providers()
+        resp = p.get('Bing', 'The Wardrobe, Narnia')
+        self.assertEqual(resp.status, 'No Results')
+    
+    def test_bing_get_found(self):
+        p = Providers()
+        resp = p.get('Bing', '301 Front Street West, Toronto')
+        self.assertEqual(resp.status, 'OK')
+        self.assertAlmostEqual(resp.data['lat'], 43.64, 2)
+        self.assertAlmostEqual(resp.data['lng'], -79.39, 2)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_google.py
@@ -0,0 +1,19 @@
+import unittest
+from providers import Providers
+
+class TestGoogle(unittest.TestCase):
+
+    def test_google_get_not_found(self):
+        p = Providers()
+        resp = p.get('Google', 'The Wardrobe, Narnia')
+        self.assertEqual(resp.status, 'No Results')
+    
+    def test_google_get_found(self):
+        p = Providers()
+        resp = p.get('Google', '301 Front Street West, Toronto')
+        self.assertEqual(resp.status, 'OK')
+        self.assertAlmostEqual(resp.data['lat'], 43.64, 2)
+        self.assertAlmostEqual(resp.data['lng'], -79.39, 2)
+
+if __name__ == '__main__':
+    unittest.main()
